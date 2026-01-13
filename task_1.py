0bc3dd29a60eb9c6083a26de4a4e519338779c99
sashakudriavtsew-design <sasha.kudriavtsew@yandex.ru>
2026-01-13 21:33:22 +0300

    Лабораторная работа 3

---
+++ task_1.py
@@ -0,0 +1,17 @@
+# TODO Напишите функцию для поиска индекса товара
+def find_item_index(items, item):
+    """Функция для поиска первого индекса товара в списке"""
+    try:
+        return items.index(item)
+    except ValueError:
+        return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_index(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
